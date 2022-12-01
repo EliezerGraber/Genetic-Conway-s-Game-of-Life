d74d03eb1dfb512126b91775fd22c8dca0d6c425
esg <57239843+EliezerGraber@users.noreply.github.com>
2022-12-01 18:49:53 +0000

    Finished basic game

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import copy
+
 class World:
   def __init__(self, x, y):
     self.rows = x
@@ -11,26 +13,41 @@ class World:
        print()
 
   def evolve(self):
-    pass
+    old_cells = copy.deepcopy(self.cells)
+    for x, colls in enumerate(old_cells):
+      for y, cell in enumerate(colls):
+        count = self.neighbor_count(x, y, old_cells)
+        #print(count, end = "")
+        if (count == 3 and old_cells[x][y] == 0) or ((count > 3 or count < 2) and old_cells[x][y] == 1):
+          self.toggle_cell(x, y)
+      #print()
 
-  def neighbor_count(self, x, y):
+  def neighbor_count(self, x, y, c):
     count = 0
     for i in range(x - 1, x + 2):
       for j in range(y - 1, y + 2):
-        if i < 0 or i > self.rows or j < 0 or j > self.cols or (i == x and j == y):
+        if i < 0 or i >= self.rows or j < 0 or j >= self.cols or (i == x and j == y):
           continue
-        if self.cells[i][j] == 1:
+        #print(x,y,i,j,c[i][j])
+        if c[i][j] == 1:
+          #print(x,y,i,j)
           count +=1
     return count
 
   def toggle_cell(self, x, y):
-     self.cells[x][y] = self.cells[x][y] - 1
+     self.cells[x][y] = 1 - self.cells[x][y]
 
 
 
 
 world = World(10, 10)
-world.toggle_cell(2,3)
-world.print_world()
-print(world.neighbor_count(2,2))
+world.toggle_cell(4,4)
+world.toggle_cell(4,5)
+world.toggle_cell(4,6)
+world.toggle_cell(5,5)
+for x in range(15):
+  world.print_world()
+  world.evolve()
+  print()
+print(world.neighbor_count(2,3,world.cells))
 
